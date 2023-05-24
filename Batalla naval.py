78057f53d6b5aa6095fbfeda9fe96fdcdfb1c320
Franco Pizzolante <Francogpizzolantesuarez@gmail.com>
2023-05-24 11:17:43 -0300

    Add files via upload

---
+++ Batalla naval.py
@@ -0,0 +1,11 @@
+barco_j1_letra = ""
+barco_j2_numero = ""
+
+barco_j1_numero = ""
+barco_j2_letra = ""
+
+cordenada_numero = [1,2,3,4,5,6]
+cordenada_letra =[" ","A","B","C","D","E","F"]
+print ("[   ]","[ A ]","[ B ]","[ C ]","[ D ]","[ E ]","[ F ]")
+for i in range (6):
+    print ("[",(cordenada_numero[i]),"]")
