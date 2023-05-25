d1f04c00e3aa484ed8e772bf9b1d15b6e97bb762
Franco Pizzolante <Francogpizzolantesuarez@gmail.com>
2023-05-25 12:18:30 -0300

    Update Batalla naval.py

--- Batalla naval.py
+++ Batalla naval.py
@@ -1,15 +1,12 @@
 #import
 import random
+
 #Implementar una función que permita ubicar la cantidad de enemigos recibidos por parámetro. 
-def ubicacion_enemigos(cantidad_de_enemigos,cordenadas_enemigos_x,cordenadas_enemigos_y,cantidad):
-    for i in range (cantidad_de_enemigos):
-        x=random.randint(1,cantidad)
-        y=random.randint(1,cantidad)
-        cordenadas_enemigos_x.append(x)
-        cordenadas_enemigos_y.append(y)
+def ubicacion_enemigos(cordenadas_enemigos_x,cordenadas_enemigos_y):
     print (cordenadas_enemigos_x)
     print (cordenadas_enemigos_y)
 #fin def ubicacion
+
 #Creacion de tablero
 def tablero(linea,cantidad,cordenada_letra):
     cantidad +=1
@@ -19,38 +16,45 @@ def tablero(linea,cantidad,cordenada_letra):
         print (linea)
         linea =""
 #fin def tablero
-def disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra):
-    for i in range (cantidad_disparos):
-        cordenadas_disparo = str(input("Ingrese una cordenada de la siguiente manera (LETRA,NUMERO)"))
 
-        cordenadas_disparo_x_simp = (cordenadas_disparo[0])
+#DISPAROS
+def disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra,cordenadas_disparo):
 
-        cordenadas_disparo_x.append (int(cordenada_letra[0].index(" "+ cordenadas_disparo_x_simp + " ")))
-        cordenadas_disparo_y.append (int(cordenadas_disparo[1]))
+        cordenadas_disparo_x = (int(cordenada_letra[0].index(" "+ cordenadas_disparo[0] + " ")))
+        cordenadas_disparo_y = (int(cordenadas_disparo[1]))
         
         print(cordenadas_disparo_x)
         print(cordenadas_disparo_y)
         
-        for e in 
-        if (cordenadas_disparo_x == cordenadas_enemigos_x) and (cordenadas_disparo_y == cordenadas_enemigos_y):
-            return True
+        for e in range (cantidad_disparos):
+            if (cordenadas_disparo_x == cordenadas_enemigos_x[e]) and (cordenadas_disparo_y == cordenadas_enemigos_y[e]):
+                return True
+#FIN DISPAROS
 
-#ubicacion
-cantidad_de_enemigos= int(input("Ingrese la cantidad de enemigos a elección"))
-cordenadas_enemigos_x=[]
-cordenadas_enemigos_y=[]
 #tablero
 cordenada_letra =[[" "," A "," B "," C "," D "," E "," F "," G "," H "," I "," J "],["1"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["2"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["3"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["4"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["5"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["6"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["7"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["8"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["9"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["10"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "]]
 linea =""
 cantidad = int(input("Ingrese el tamaño del tablero:"))
+#ubicacion
+cantidad_de_enemigos= int(input("Ingrese la cantidad de enemigos a elección"))
+cordenadas_enemigos_x=[]
+cordenadas_enemigos_y=[]
+for i in range (cantidad_de_enemigos):
+    x=random.randint(1,cantidad)
+    y=random.randint(1,cantidad)
+    cordenadas_enemigos_x.append(x)
+    cordenadas_enemigos_y.append(y)
 #Disparos
-cantidad_disparos=5
-cordenadas_disparo_x=[]
-cordenadas_disparo_y=[]
-
-
+cantidad_disparos=2
+cordenadas_disparo_x=0
+cordenadas_disparo_y=0
 
 #LLAMAR FUNCIONES
+for i in range (cantidad_disparos):
+    tablero(linea,cantidad,cordenada_letra) 
+    ubicacion_enemigos(cordenadas_enemigos_x,cordenadas_enemigos_y)
+    cordenadas_disparo = str(input("Ingrese una cordenada de la siguiente manera (LETRA,NUMERO)"))
+    disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra,cordenadas_disparo)
+    
 tablero(linea,cantidad,cordenada_letra) 
-ubicacion_enemigos(cantidad_de_enemigos,cordenadas_enemigos_x,cordenadas_enemigos_y,cantidad)
-disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra)
+ubicacion_enemigos(cordenadas_enemigos_x,cordenadas_enemigos_y)
