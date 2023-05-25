e7caacdf5da94789e3a2f02ed67d51bf29ac6228
Franco Pizzolante <Francogpizzolantesuarez@gmail.com>
2023-05-25 11:40:37 -0300

    Update Batalla naval.py

--- Batalla naval.py
+++ Batalla naval.py
@@ -1,20 +1,56 @@
+#import
+import random
 #Implementar una función que permita ubicar la cantidad de enemigos recibidos por parámetro. 
-def ubicacion(x):
-    x = int(input("deposite la cantidad de enemigos a elección"))
-    for i in range (codenadas_letras):
-        randint: ["A","B","C","D","E","F","G","H","I","J"],["1","2","3","","","",",","","","",",","",])
+def ubicacion_enemigos(cantidad_de_enemigos,cordenadas_enemigos_x,cordenadas_enemigos_y,cantidad):
+    for i in range (cantidad_de_enemigos):
+        x=random.randint(1,cantidad)
+        y=random.randint(1,cantidad)
+        cordenadas_enemigos_x.append(x)
+        cordenadas_enemigos_y.append(y)
+    print (cordenadas_enemigos_x)
+    print (cordenadas_enemigos_y)
+#fin def ubicacion
+#Creacion de tablero
+def tablero(linea,cantidad,cordenada_letra):
+    cantidad +=1
+    for e in range (cantidad):
+        for i in range (cantidad):
+            linea += (cordenada_letra[e][i])+""
+        print (linea)
+        linea =""
+#fin def tablero
+def disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra):
+    for i in range (cantidad_disparos):
+        cordenadas_disparo = str(input("Ingrese una cordenada de la siguiente manera (LETRA,NUMERO)"))
 
-barco_j1_letra = ""
-barco_j2_numero = ""
+        cordenadas_disparo_x_simp = (cordenadas_disparo[0])
 
-barco_j1_numero = ""
-barco_j2_letra = ""
+        cordenadas_disparo_x.append (int(cordenada_letra[0].index(" "+ cordenadas_disparo_x_simp + " ")))
+        cordenadas_disparo_y.append (int(cordenadas_disparo[1]))
+        
+        print(cordenadas_disparo_x)
+        print(cordenadas_disparo_y)
+        
+        for e in 
+        if (cordenadas_disparo_x == cordenadas_enemigos_x) and (cordenadas_disparo_y == cordenadas_enemigos_y):
+            return True
+
+#ubicacion
+cantidad_de_enemigos= int(input("Ingrese la cantidad de enemigos a elección"))
+cordenadas_enemigos_x=[]
+cordenadas_enemigos_y=[]
+#tablero
+cordenada_letra =[[" "," A "," B "," C "," D "," E "," F "," G "," H "," I "," J "],["1"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["2"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["3"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["4"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["5"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["6"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["7"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["8"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["9"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "],["10"," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "]]
 linea =""
-cantidad = int(input("Ingrese el tamanio del tablero:"))
-cordenada_letra =[[" ","A","B","C","D","E","F","G","H","I","J"],["1"," "," "," "," "," "," "," "," "," "," "],["2"," "," "," "," "," "," "," "," "," "," "],["3"," "," "," "," "," "," "," "," "," "," "],["4"," "," "," "," "," "," "," "," "," "," "],["5"," "," "," "," "," "," "," "," "," "," "],["6"," "," "," "," "," "," "," "," "," "," "],["7"," "," "," "," "," "," "," "," "," "," "],["8"," "," "," "," "," "," "," "," "," "," "],["9"," "," "," "," "," "," "," "," "," "," "],["10"," "," "," "," "," "," "," "," "," "," "]]
-cantidad +=1
-for e in range (cantidad):
-    for i in range (cantidad):
-        linea += (cordenada_letra[e][i])+" "
-    print (linea)
-    linea =""
+cantidad = int(input("Ingrese el tamaño del tablero:"))
+#Disparos
+cantidad_disparos=5
+cordenadas_disparo_x=[]
+cordenadas_disparo_y=[]
+
+
+
+#LLAMAR FUNCIONES
+tablero(linea,cantidad,cordenada_letra) 
+ubicacion_enemigos(cantidad_de_enemigos,cordenadas_enemigos_x,cordenadas_enemigos_y,cantidad)
+disparos(cantidad_disparos,cordenadas_disparo_x,cordenadas_disparo_y,cordenadas_enemigos_x,cordenadas_enemigos_y,cordenada_letra)
