78047ef6eff8bd84c7f3e518e78e37084f03931b
Franco Pizzolante <Francogpizzolantesuarez@gmail.com>
2023-05-25 23:01:48 -0300

    Update Batalla naval.py

--- Batalla naval.py
+++ Batalla naval.py
@@ -1,21 +1,22 @@
+
 import random
 
-# Función para imprimir la ubicación de los enemigos
-def ubicacion_enemigos(cordenadas_enemigos_x, cordenadas_enemigos_y):
-    print(cordenadas_enemigos_x)
-    print(cordenadas_enemigos_y)
-
 # Función para crear el tablero
-def tablero(linea, cantidad, cordenada_letra):
+def crear_tablero(linea, cantidad, coordenadas_letras):
     cantidad += 1
     for e in range(cantidad):
         for i in range(cantidad):
-            linea += cordenada_letra[e][i] + " "
+            linea += coordenadas_letras[e][i] + " "
         print(linea)
         linea = ""
 
+# Función para imprimir la ubicación de los enemigos
+def imprimir_ubicacion_enemigos(enemigos_x, enemigos_y):
+    print(enemigos_x)
+    print(enemigos_y)
+
 # Función para determinar la dificultad
-def dificultad(cantidad, nivel):
+def determinar_cantidad_disparos(cantidad, nivel):
     casillas = cantidad ** 2
     if nivel == 1:
         cantidad_disparos = int((70 * casillas) / 100)
@@ -26,31 +27,31 @@ def dificultad(cantidad, nivel):
     return cantidad_disparos
 
 # Función para manejar los disparos
-def disparos(cantidad_disparos, cordenadas_disparo_x, cordenadas_disparo_y, cordenadas_enemigos_x, cordenadas_enemigos_y, cordenada_letra, cordenadas_disparo):
-    cordenadas_disparo_x = int(cordenada_letra[0].index(" " + cordenadas_disparo[0] + " "))
-    cordenadas_disparo_y = int(cordenadas_disparo[1])
+def realizar_disparo(disparo_x, disparo_y, enemigos_x, enemigos_y, coordenadas_letras, cordenadas_disparo):
+    disparo_x = int(coordenadas_letras[0].index(" " + cordenadas_disparo[0] + " "))
+    disparo_y = int(cordenadas_disparo[1])
 
     for e in range(cantidad_de_enemigos):
-        if cordenadas_disparo_x == cordenadas_enemigos_x[e] and cordenadas_disparo_y == cordenadas_enemigos_y[e]:
+        if disparo_x == enemigos_x[e] and disparo_y == enemigos_y[e]:
             print("TRUE")
-            cordenada_letra[cordenadas_disparo_y][cordenadas_disparo_x] = " x "
+            coordenadas_letras[disparo_y][disparo_x] = " x "
             break
         if e == cantidad_de_enemigos - 1:
             print("FALSE")
-            cordenada_letra[cordenadas_disparo_y][cordenadas_disparo_x] = " o "
+            coordenadas_letras[disparo_y][disparo_x] = " o "
 
 # Creación del tablero
-cordenada_letra = [["  ", " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "],
-                   [" 1", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 2", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 3", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 4", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 5", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 6", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 7", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 8", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   [" 9", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
-                   ["10", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "]]
+coordenadas_letras = [["  ", " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J "],
+                      [" 1", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 2", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 3", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 4", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 5", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 6", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 7", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 8", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      [" 9", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "],
+                      ["10", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - ", " - "]]
 
 linea = ""
 cantidad = int(input("Ingrese el tamaño del tablero:"))
@@ -60,27 +61,33 @@ nivel = int(input("Ingrese la dificultad (1 = fácil)(2 = medio)(3 = difícil):"
 
 # Ubicación de los enemigos
 cantidad_de_enemigos = int(input("Ingrese la cantidad de enemigos a elección:"))
-cordenadas_enemigos_x = []
-cordenadas_enemigos_y = []
+enemigos_x = []
+enemigos_y = []
 
 for i in range(cantidad_de_enemigos):
     x = random.randint(1, cantidad)
     y = random.randint(1, cantidad)
-    cordenadas_enemigos_x.append(x)
-    cordenadas_enemigos_y.append(y)
+    enemigos_x.append(x)
+    enemigos_y.append(y)
 
 # Disparos
-cantidad_disparos = dificultad(cantidad, nivel)
-cordenadas_disparo_x = 0
-cordenadas_disparo_y = 0
+cantidad_disparos = determinar_cantidad_disparos(cantidad, nivel)
+disparo_x = 0
+disparo_y = 0
 
 # Llamada a las funciones
 for i in range(cantidad_disparos):
-    tablero(linea, cantidad, cordenada_letra)
-    ubicacion_enemigos(cordenadas_enemigos_x, cordenadas_enemigos_y)
-    cordenadas_disparo = str(input("Ingrese una coordenada de la siguiente manera (LETRA,NUMERO):"))
-    disparos(cantidad_disparos, cordenadas_disparo_x, cordenadas_disparo_y, cordenadas_enemigos_x, cordenadas_enemigos_y, cordenada_letra, cordenadas_disparo)
+    crear_tablero(linea, cantidad, coordenadas_letras)
+    imprimir_ubicacion_enemigos(enemigos_x, enemigos_y)
+    coordenadas_disparo = str(input("Ingrese una coordenada de la siguiente manera (LETRA,NUMERO):"))
+    while True:
+        if len(coordenadas_disparo) != 2 or coordenadas_disparo[0].isalpha() == False or coordenadas_disparo[1].isdigit() == False:
+            print("Coordenadas inválidas. Vuelva a intentarlo.")
+            coordenadas_disparo = input("Ingrese una coordenada de la siguiente manera (LETRA,NUMERO):")
+        else:
+            break
+    realizar_disparo(disparo_x, disparo_y, enemigos_x, enemigos_y, coordenadas_letras, coordenadas_disparo)
 
 # Mostrar el tablero final
-tablero(linea, cantidad, cordenada_letra)
-ubicacion_enemigos(cordenadas_enemigos_x, cordenadas_enemigos_y)
+crear_tablero(linea, cantidad, coordenadas_letras)
+imprimir_ubicacion_enemigos(enemigos_x, enemigos_y)
